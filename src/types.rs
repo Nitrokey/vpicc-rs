e9a532c442d1d20c0450e6a370f3e77197c64d1b
Guinness <oudin@crans.org>
2021-09-14 09:35:44 +0200

    Fix clippy and compilation warnings

--- src/types.rs
+++ src/types.rs
@@ -5,6 +5,7 @@ use log::{info, warn};
 
 use crate::constants::*;
 
+#[derive(Default)]
 pub struct SmartCard {
     host : &'static str,
     port : u16,
@@ -19,14 +20,14 @@ impl SmartCard {
         }
     }
 
-    fn getATR(&self) -> [u8; 10] {
+    fn get_atr(&self) -> [u8; 10] {
         // For now the ATR value is a constant, logic will be implemented later.
-        return ATR_VALUE;
+        ATR_VALUE
     }
     // Nothing to do
-    fn powerOn(&self) { println!("Power On");}
+    fn power_on(&self) { println!("Power On");}
     // Nothing to do
-    fn powerOff(&self) {println!("Power Off");}
+    fn power_off(&self) {println!("Power Off");}
     // Nothing to do
     fn reset(&self) {println!("Reset");}
     // Not implemented
@@ -36,23 +37,32 @@ impl SmartCard {
 
         loop {
             let mut _size = [0, 0];
-            stream.read(&mut _size).expect("VirtualPCD shut down.");
+            let byte_read = stream.read(&mut _size).expect("VirtualPCD shut down.");
             let size = _size[1];
-            let mut msg = [0];
-            stream.read(&mut msg).expect("VirtualPCD shut down.");
-            println!("{:?}", msg);
+            if byte_read == 0 {
+                break;
+            }
+            let mut msg = [0; 128];
+            let byte_read =stream.read(&mut msg).expect("VirtualPCD shut down.");
+            if byte_read == 0 {
+                break;
+            }
 
             if size == VPCD_CTRL_LEN {
                 match msg[0] {
-                    VPCD_CTRL_OFF => self.powerOff(),
-                    VPCD_CTRL_ON => self.powerOn(),
+                    VPCD_CTRL_OFF => self.power_off(),
+                    VPCD_CTRL_ON => self.power_on(),
                     VPCD_CTRL_RESET => self.reset(),
                     VPCD_CTRL_ATR => {
                         println!("Sending ATR");
-                        let atr = self.getATR();
+                        let atr = self.get_atr();
                         // [0x00, size, ;, atr
                         let msg_o = [&[0x00], &[11], &[59], &atr[..]].concat();
-                        stream.write(&msg_o).expect("Unable to send the message. Virtual PCD shut down,");
+                        let bytes_written = stream.write(&msg_o).expect("Unable to send the message. Virtual PCD shut down,");
+                        if bytes_written == 0 {
+                            println!("Error sending ATR");
+                            break;
+                        }
                         println!("Finished sending ATR");
                     },
                     _ => warn!("Unknown command"),
@@ -61,6 +71,7 @@ impl SmartCard {
                 println!("Virtual PCD Shut down.");
                 break;
             } else {
+                self.execute();
                 println!("Wrong size received");
             }
         }
