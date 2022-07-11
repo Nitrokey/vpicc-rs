1b03a6d10fa6eb26dba4f941a8662ed6dea638ef
Robin Krahl <robin@nitrokey.com>
2022-07-11 17:39:30 +0200

    Simplify control flow and command parsing

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,9 @@
 use std::io::prelude::*;
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
 use std::fmt::Display;
 use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
 
-use log::{warn, info, debug, trace};
+use log::{info, debug, trace};
 
 use crate::constants::*;
 
@@ -33,55 +33,51 @@ pub struct Connection {
 
 impl Connection {
     pub fn run<V: VSmartCard>(mut self, card: &mut V) -> Result<()> {
-        while self.poll(card)? {}
-        Ok(())
+        loop {
+            self.poll(card)?;
+        }
     }
 
-    pub fn poll<V: VSmartCard>(&mut self, card: &mut V) -> Result<bool> {
-        let mut size = [0, 0];
-        let byte_read = self.stream.read(&mut size)?;
-        let size = usize::from(u16::from_be_bytes(size));
-        if byte_read == 0 {
-            return Ok(false);
+    pub fn poll<V: VSmartCard>(&mut self, card: &mut V) -> Result<()> {
+        let msg = self.read()?;
+        if msg.is_empty() {
+            return Err(Error::new(ErrorKind::Other, "received an empty message"));
         }
 
-        let mut msg = vec![0u8; size];
-        self.stream.read_exact(&mut msg)?;
-
-        trace!("MSG : {:?}", msg);
-
-        if size == VPCD_CTRL_LEN {
-            match msg[0] {
-                VPCD_CTRL_OFF => card.power_off(),
-                VPCD_CTRL_ON => card.power_on(),
-                VPCD_CTRL_RESET => card.reset(),
-                VPCD_CTRL_ATR => {
+        if msg.len() == 1 {
+            match Command::try_from(msg[0])? {
+                Command::PowerOff => card.power_off(),
+                Command::PowerOn => card.power_on(),
+                Command::Reset => card.reset(),
+                Command::GetAtr => {
                     debug!("Sending ATR");
-                    self.send(&card.atr())?;
+                    self.send(card.atr())?;
                 },
-                _ => warn!("Unknown command"),
             }
-        } else if size == 0 {
-            info!("Virtual PCD Shut down.");
-            return Ok(false);
         } else {
             debug!("APDU received");
-            trace!("received: {:x?}", msg);
             let response = card.execute(&msg);
-            trace!("response: {:X?}", response);
             self.send(&response)?;
         }
 
-        Ok(true)
+        Ok(())
+    }
+
+    fn read(&mut self) -> Result<Vec<u8>> {
+        let mut size = [0, 0];
+        self.stream.read_exact(&mut size)?;
+        let size = usize::from(u16::from_be_bytes(size));
+        let mut msg = vec![0u8; size];
+        self.stream.read_exact(&mut msg)?;
+        trace!("received message: {:x?}", msg);
+        Ok(msg)
     }
 
     fn send(&mut self, data: &[u8]) -> Result<()> {
+        trace!("sending message: {:x?}", data);
         let size = (data.len() as u16).to_be_bytes();
         let msg = &[&size[..], data].concat();
-        let bytes_written = self.stream.write(msg)?;
-        if bytes_written < msg.len() {
-            panic!("failed to write all data");
-        }
+        self.stream.write_all(msg)?;
         Ok(())
     }
 }
@@ -89,6 +85,28 @@ impl Connection {
 impl From<TcpStream> for Connection {
     fn from(stream: TcpStream) -> Self {
         Self { stream }
+    }
+}
+
+enum Command {
+    PowerOff,
+    PowerOn,
+    Reset,
+    GetAtr,
+}
+
+impl TryFrom<u8> for Command {
+    type Error = Error;
+
+    fn try_from(command: u8) -> Result<Self> {
+        // https://frankmorgner.github.io/vsmartcard/virtualsmartcard/api.html
+        match command {
+            0 => Ok(Self::PowerOff),
+            1 => Ok(Self::PowerOn),
+            2 => Ok(Self::Reset),
+            4 => Ok(Self::GetAtr),
+            _ => Err(Error::new(ErrorKind::InvalidData, format!("unsupported control command {}", command))),
+        }
     }
 }
 
